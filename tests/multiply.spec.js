91da122d1c01ac8c8e9d46035cf6ff5b4e496042
Nina Nyberg <nina.b.nyberg@gmail.com>
2022-01-31 12:19:49 +0200

    Bonus iteration added

--- tests/multiply.spec.js
+++ tests/multiply.spec.js
@@ -5,4 +5,14 @@ describe('Multiply', () => {
     const result = multiply(2, 3);
     expect(result).toBe(6);
   });
+
+  test('should multiply a negavite and a positive integer correctly', () => {
+    const result = multiply(-2, 3);
+    expect(result).toBe(-6);
+  });
+
+  test('should return a 0 when one of the numbers is 0', () => {
+    const result = multiply(2, 0);
+    expect(result).toBe(0);
+  });
 });
